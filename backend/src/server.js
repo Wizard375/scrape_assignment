3311a63f1732fec5014b4c262e0bc57ba4ed367f
Wizard375 <suyashsha375@gmail.com>
2026-05-07 14:22:33 +0530

    -update

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,6 +4,22 @@ const app = require("./app");
 
 const connectDB = require("./config/db");
 
-connectDB();
+const scrapeStories = require("./services/scraperService");
 
-module.exports = app;
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    await scrapeStories();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
