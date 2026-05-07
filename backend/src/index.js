ce61cbc60a65bf9b116b94eee164db9a08bbfd10
Wizard375 <suyashsha375@gmail.com>
2026-05-07 14:47:55 +0530

    update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,25 @@
 require("dotenv").config();
 
-const app = require("../src/app");
+const app = require("../src/app.js");
 
-const connectDB = require("../src/config/db");
+const connectDB = require("../src/config/db.js");
 
-connectDB();
+let isConnected = false;
 
-module.exports = app;
+module.exports = async (req, res) => {
+  try {
+    if (!isConnected) {
+      await connectDB();
+
+      isConnected = true;
+    }
+
+    return app(req, res);
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({
+      message: "Server Error",
+    });
+  }
+};
